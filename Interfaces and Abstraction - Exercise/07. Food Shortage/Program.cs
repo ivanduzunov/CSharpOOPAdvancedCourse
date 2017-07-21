378cdd9945419f62c3f8659cb5c1f870141aeae0
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-21 20:33:43 +0300

    dd

---
+++ Interfaces and Abstraction - Exercise/07. Food Shortage/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Food_Shortage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, IBuyer> buyers = new Dictionary<string, IBuyer>();
+            var n = int.Parse(Console.ReadLine());
+            var input = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+
+                if (tokens.Length == 4)
+                {
+                    buyers.Add(tokens[0],
+                        new Citizen(tokens[0], int.Parse(tokens[1]), tokens[2], tokens[3]));
+                }
+                else
+                {
+                    buyers.Add(tokens[0],
+                        new Rebel(tokens[0], int.Parse(tokens[1]), tokens[2]));
+                }
+            }
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (buyers.ContainsKey(input))
+                {
+                    buyers[input].BuyFood();
+                }
+            }
+            Console.WriteLine(buyers.Values.Sum(v => v.Food));
+        }
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/07. Food Shortage/Rebel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Food_Shortage
+{
+    public class Rebel : IBuyer
+    {
+
+        public Rebel(string name, int age, string group)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Group = group;
+            this.Food = 0;
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Group { get; private set; }
+        public int Food { get; private set; }
+        public void BuyFood()
+        {
+            this.Food += 5;
+        }
+    }
+}
