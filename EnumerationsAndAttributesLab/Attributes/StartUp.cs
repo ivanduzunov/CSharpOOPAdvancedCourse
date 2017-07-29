65cee633a9d6265a60b7d16c69e8e49c33dc7ea9
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-29 14:19:12 +0300

    EnumerationsAndAttributesLab.sln completed

---
+++ EnumerationsAndAttributesLab/Attributes/StartUp.cs
@@ -0,0 +1,11 @@
+﻿[SoftUni("Ventsi")]
+public class StartUp
+{
+    [SoftUni("Goshko")]
+    public static void Main(string[] args)
+    {
+        var tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
+
