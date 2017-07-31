2878e569371f2615d4988c4651d218edd66eda39
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-31 20:11:56 +0300

    s

--- EnumerationsAndAttributesExercise/InfernoInfinity/CustomAttribute.cs
+++ EnumerationsAndAttributesExercise/InfernoInfinity/CustomAttribute.cs
@@ -7,18 +7,20 @@ using System.Threading.Tasks;
 
 namespace InfernoInfinity
 {
+    [AttributeUsage(AttributeTargets.Class)]
     public class CustomAttribute : Attribute
     {
-        public CustomAttribute(string author, int revision, string description, List<string> reviewers)
+        public CustomAttribute(string author, int revision, string description, params string[] reviewers)
         {
             Author = author;
             Revision = revision;
             Description = description;
-            Reviewers = reviewers;
+            Reviewers = new List<string>(reviewers);
         }
-        public string Author { get; set; }
-        public int Revision { get; set; }
-        public string Description { get; set; }
-        public List<string> Reviewers { get; set; }
+        public string Author { get; private set; }
+        public int Revision { get; private set; }
+        public string Description { get; private set; }
+        public List<string> Reviewers { get; private set; }
+
     }
 }
--- EnumerationsAndAttributesExercise/InfernoInfinity/Program.cs
+++ EnumerationsAndAttributesExercise/InfernoInfinity/Program.cs
@@ -6,12 +6,39 @@ using System.Threading.Tasks;
 
 namespace InfernoInfinity
 {
-    
+    [Custom("Pesho", 3, "Used for C# OOP Advanced Course - Enumerations and Attributes.", "Pesho", "Svetlio")]
     public class Program
     {
         public static void Main(string[] args)
         {
 
+            Type type = typeof(Program);
+            var attributeValues =
+                typeof(Program).GetCustomAttributes(typeof(CustomAttribute), true).FirstOrDefault() as CustomAttribute;
+
+
+            var input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Author":
+                        Console.WriteLine($"Author: {attributeValues.Author}");
+                        break;
+                    case "Revision":
+                        Console.WriteLine($"Revision: {attributeValues.Revision}");
+                        break;
+                    case "Description":
+                        Console.WriteLine($"Class description: {attributeValues.Description}");
+                        break;
+                    case "Reviewers":
+                        Console.WriteLine($"Reviewers: {string.Join(", ", attributeValues.Reviewers)}");
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
+
     }
 }
