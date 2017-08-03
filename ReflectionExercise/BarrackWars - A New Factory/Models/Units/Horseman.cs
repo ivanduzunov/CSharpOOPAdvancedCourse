68a11dbf3987d7f2f08d404512246bf7b781b949
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-08-03 18:57:02 +0300

    dds

---
+++ ReflectionExercise/BarrackWars - A New Factory/Models/Units/Horseman.cs
@@ -0,0 +1,14 @@
+﻿
+namespace _03BarracksFactory.Models.Units
+{
+    public class Horseman : Unit
+    {
+        private const int health = 50;
+        private const int attackDamage = 10;
+
+        public Horseman()
+            : base(health, attackDamage)
+        {
+        }
+    }
+}
