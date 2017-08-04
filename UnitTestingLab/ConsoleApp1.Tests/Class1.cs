b9694bf5f0d17aa4f892134d90e3064e643dffa6
ivan.d.uzunov <ivan.d.uzunov@gmail.com>
2017-08-04 22:34:27 +0300

    UnitTestingLab.sln

---
+++ UnitTestingLab/ConsoleApp1.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestFixture]
+public class Class1
+{
+    [Test]
+    public void AxeLosesDurabilityAfterAttack()
+    {
+        //Arrange
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
+
+        //Act
+        axe.Attack(dummy);
+
+        //Assert
+        Assert.AreEqual(9, axe.DurabilityPoints);
+    }
+}
+
---
+++ UnitTestingLab/ConsoleApp1.Tests/DummyTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+[TestFixture]
+public class DummyTests
+{
+    private const int AxeAttack = 10;
+    private const int AxeDurability = 10;
+    private const int DummyHp = 10;
+    private const int DummyExp = 100;
+
+    private Axe axe;
+    private Dummy dummy;
+
+    [SetUp]
+    public void Testinit()
+    {
+        this.axe = new Axe(AxeAttack, AxeDurability);
+        this.dummy = new Dummy(DummyHp, DummyExp);
+    }
+
+    [Test]
+    public void DummyLosesHealthIfAtacked()
+    {
+        //Arrange
+        axe = new Axe(10, 10);
+        dummy = new Dummy(12, 10);
+
+        //Act
+        axe.Attack(dummy);
+
+        //Assert
+        Assert.AreEqual(2, dummy.Health);
+    }
+    [Test]
+    public void DeadDummyThrowsExceptiojIfAtacked()
+    {
+        //Arrange
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(0, 10);
+
+        //Act
+        var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+
+        //Assert
+        Assert.That(ex.Message, Is.EqualTo("Dummy is dead."));
+    }
+
+    [Test]
+    public void DeadDummyCanGiveXP()
+    {
+        Dummy dummy = new Dummy(0, 10);
+
+        int exp = dummy.GiveExperience();
+
+        Assert.AreEqual(10, exp, "Dead dummy gives exp!");
+    }
+
+    [Test]
+    public void ActiveDummyCantGiveXP()
+    {
+        Dummy dummy = new Dummy(10, 10);
+
+        var ex = Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+
+        Assert.AreEqual(ex.Message, "Target is not dead.");
+    }
+}
+
